7b8f192592634c062ba2255ffd4f6bf01b6b4e56
Philip Yaw Boahene <boahenep22@gmail.com>
2024-10-01 11:41:20 -0700

    exe 3 completed

--- ex03.js
+++ ex03.js
@@ -2,4 +2,17 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+const findMissingNumber = function (arr) {
+  let answer = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === i + 1) {
+      continue;
+    } else {
+      answer = i + 1;
+    }
+    break;
+  }
+  return answer;
+};
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
