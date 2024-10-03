c792e6c1f5720a481abbabb4341b2241c09f13f4
Philip Yaw Boahene <boahenep22@gmail.com>
2024-10-02 20:25:04 -0700

    exe 4 completed

--- ex04.js
+++ ex04.js
@@ -1,5 +1,20 @@
 // Exercise 4: Write a function `mostFrequentChar` that takes a string and returns the character
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
+function mostFrequentChar(str) {
+  const wordArr = str.split("");
+  const charObj = {};
+  let largestCountedChar = "";
+  for (let i = 0; i <= wordArr.length; i++) {
+    if (!Object.keys(charObj).includes(wordArr[i])) {
+      charObj[wordArr[i]] = 1;
+    } else {
+      charObj[wordArr[i]]++;
+      largestCountedChar = wordArr[i];
+    }
+  }
+
+  return largestCountedChar;
+}
 
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
