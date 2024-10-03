9c7761e029c7c3966daab02a7aa7928e685cf7a0
Philip Yaw Boahene <boahenep22@gmail.com>
2024-10-02 20:25:28 -0700

    exe 5 completed

--- ex05.js
+++ ex05.js
@@ -2,4 +2,18 @@
 // and returns the number of consonants in the string.
 // Example: countConsonants("hello world") should return 7.
 
+function countConsonants(str) {
+  const consonantList = "bcdfghjklmnpqrstvwxyz";
+  let countedConsonantsNumbers = 0;
+  const chartArr = str.split("");
+  for (let i = 0; i <= chartArr.length; i++) {
+    if (consonantList.includes(chartArr[i])) {
+      countedConsonantsNumbers++;
+    } else {
+      continue;
+    }
+  }
+  return countedConsonantsNumbers;
+}
+
 console.log(countConsonants("hello world")); // Expected output: 7
